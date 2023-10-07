9d660158e4ce79ede01e791cb36f2e25ee6e152c
huangbo <qqjay2013@gmail.com>
2023-10-07 17:54:22 +0800

    feat: 测试turbo打包11111

--- www/app/components/navbar/Logo.tsx
+++ www/app/components/navbar/Logo.tsx
@@ -1,20 +1,9 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-
-import { useRouter } from "next/navigation";
+import Image from 'next/image'
 
 const Logo = () => {
-  const router = useRouter();
-  return (
-    <Image
-      alt="Logo"
-      className="hidden md:block cursor-pointer"
-      height="100"
-      width="100"
-      src="/images/logo.png"
-    />
-  );
-};
+  return <Image alt="Logo" className="hidden md:block cursor-pointer" height="100" width="100" src="/images/logo.png" />
+}
 
-export default Logo;
+export default Logo
